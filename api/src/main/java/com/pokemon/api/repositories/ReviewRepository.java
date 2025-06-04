5e634c3be9518a6cf3cb004d2955257c80daacd3
salloum91 <aboward70@gmail.com>
2025-06-03 17:30:55 -0700

    Remove api folder

--- api/src/main/java/com/pokemon/api/repositories/ReviewRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pokemon.api.repositories;
-
-import com.pokemon.api.model.Review;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReviewRepository extends JpaRepository<Review, Integer> {
-}
